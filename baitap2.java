9b17f95fb01f919053665ef5f7359aadb5b2f9b2
thanhson5 <154438080+thanhson5@users.noreply.github.com>
2024-11-06 22:43:52 +0700

    Update baitap2.java

--- baitap2.java
+++ baitap2.java
@@ -4,12 +4,12 @@ public class FibonacciSequence {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Nhập số phần tử của dãy Fibonacci: ");
+        System.out.print("Nhap so phan tu cua day Fibonacci: ");
         int n = scanner.nextInt();
         
         int first = 0, second = 1;
 
-        System.out.println("Dãy Fibonacci với " + n + " phần tử đầu tiên:");
+        System.out.println("Day Fibonacci voi " + n + " phan tu dau tien:");
         
         for (int i = 1; i <= n; i++) {
             System.out.print(first + " ");
